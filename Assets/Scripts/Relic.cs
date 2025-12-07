7640f21051d717015dea982d7c1e03646300c0b1
AbbassRaza <lashariabbasraza@gmail.com>
2025-12-07 15:28:00 +0500

    Remove debug logs from Relic script
    Cleaned up the Relic.cs script by removing debug log statements used for development and troubleshooting. This streamlines the code for production use and reduces console clutter.

--- Assets/Scripts/Relic.cs
+++ Assets/Scripts/Relic.cs
@@ -7,36 +7,21 @@ public class Relic : MonoBehaviour
 
     private bool playerInRange = false;
 
-    // --- NEW DEBUG SECTION ---
-    void Start()
-    {
-        // If this doesn't print, the script is disabled or not attached!
-        Debug.Log("RELIC SCRIPT IS ALIVE on object: " + gameObject.name);
-    }
-    // -------------------------
-
     void Update()
     {
-        // Now checks if Player is close AND presses 'E'
+        // Check if Player is close AND presses 'E'
         if (playerInRange && Input.GetKeyDown(KeyCode.E))
         {
-            Debug.Log("E key pressed while in range. Picking up relic.");
             PickUpRelic();
         }
     }
 
     void PickUpRelic()
     {
-        Debug.Log("PickUpRelic called.");
-
         // 1. Unlock the Time Travel ability
         if (TimeManager.instance != null)
         {
             TimeManager.instance.UnlockAbility();
-        }
-        else
-        {
-            Debug.LogError("TimeManager instance not found!");
         }
 
         // 2. Show the tutorial text
@@ -44,27 +29,17 @@ public class Relic : MonoBehaviour
         {
             DialogueManager.instance.ShowDialogue(message);
         }
-        else
-        {
-            Debug.LogError("DialogueManager instance not found!");
-        }
 
-        // 3. Destroy the relic (so you can't pick it up twice)
+        // 3. Destroy the relic
         Destroy(gameObject);
     }
 
     // Detect when player walks close
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // --- NEW DEBUG ---
-        // This prints the name of ANYTHING that touches the relic
-        Debug.Log("Collision detected with: " + collision.gameObject.name);
-        // -----------------
-
         if (collision.CompareTag("Player"))
         {
             playerInRange = true;
-            Debug.Log("Player entered Relic range. Press E to interact.");
         }
     }
 
@@ -74,7 +49,6 @@ public class Relic : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             playerInRange = false;
-            Debug.Log("Player left Relic range.");
         }
     }
 }
